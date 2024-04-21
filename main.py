8ed2bd45ceaa0a88c39b9c85265864496000f8b4
Pritam Kumar Singh <166985209+pritamsingh3202@users.noreply.github.com>
2024-04-21 22:36:27 +0530

    main.py

--- main.py
+++ main.py
@@ -1,34 +1,36 @@
-import numpy as np
-from numpy.linalg import norm
+import os
+
 import tensorflow
 from tensorflow.keras.preprocessing import image
-from tensorflow.keras.layers import GlobalMaxPool2D
-from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input
-import pickle
-import streamlit as st
-import os
+from tensorflow.keras.layers import GlobalMaxPooling2D
+from keras.applications.vgg16 import VGG16,  preprocess_input
+# from keras.src.applications.vgg16 import VGG16
+# from keras.src.applications.vgg16 import preprocess_input
+import numpy as np
+from numpy.linalg import norm
+from tqdm import tqdm
 from PIL import Image
-from sklearn.neighbors import NearestNeighbors
 
-feature_list = np.array(pickle.load(open('embeddings.pkl', 'rb')))
-filenames = pickle.load(open('filenames.pkl', 'rb'))
+import pickle
+
+Image.LOAD_TRUNCATED_IMAGES = True
 
 
-model = ResNet50(weights= 'imagenet', include_top= False, input_shape=(224, 224, 3))
+
+
+
+
+model = VGG16(weights='imagenet',include_top=False,input_shape=(224,224,3))
 model.trainable = False
 
-model = tensorflow.keras.Sequential([ model, GlobalMaxPool2D()])
+model = tensorflow.keras.Sequential([
+    model,
+    GlobalMaxPooling2D()
+])
 
-st.title('FASHION RECOMMENDER SYSTEM')
 
+# print(model.summary())
 
-def save_uploaded_file(uploaded_file):
-    try:
-        with open(os.path.join('uploads', uploaded_file.name), 'wb') as f:
-            f.write(uploaded_file.getvalue())
-        return 1
-    except:
-        return 0
 def features_extraction(img_path, model):
     img = image.load_img(img_path, target_size=(224, 224))
     img_array = image.img_to_array(img)
@@ -39,27 +41,24 @@ def features_extraction(img_path, model):
 
     return normalized_result
 
-def recommend(features, feature_list):
-    neighbors = NearestNeighbors(n_neighbors=7, algorithm='brute', metric='euclidean')
-    neighbors.fit(feature_list)
+filenames = []
 
-    distances, indices = neighbors.kneighbors([features])
-# file upload
-uploaded_file = st.file_uploader("Choose an image")
-if uploaded_file is not None:
-    if save_uploaded_file(uploaded_file):
-        display_image = Image.open(uploaded_file)
-        st.image(display_image)
+for file in os.listdir('Images'):
+    filenames.append(os.path.join('Images', file))
 
-        features = features_extraction(os.path.join("uploads", uploaded_file.name), model)
-        st.text(features)
+# print(len(filenames))
+# print(filenames[0:5])
+# print(os.listdir('Images'))
 
-        indices = recommend(features, feature_list)
+features_list =[]
 
-        col1, col2, col3, col4, col5, col6, col7 = st.beta_columns(7)
+for file in tqdm(filenames):
+    try:
+        features_list.append(features_extraction(file, model))
+    except OSError:
+        print(f"Skipping corrupted image {file}")
 
-        with col1:
-            st.image(filenames[indices[0][0]])
-    else:
-        st.header("Some error occurred in file upload")
+# print(np.array(features_list).shape)
 
+pickle.dump(features_list, open('embedding.pkl', 'wb'))
+pickle.dump(filenames, open('filenames.pkl', 'wb'))
