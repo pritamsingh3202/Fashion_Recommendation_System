fcaba610934108fafc980b4f07bc05d8ed71cdf9
Pritam Kumar Singh <166985209+pritamsingh3202@users.noreply.github.com>
2024-04-21 22:35:46 +0530

    app.py

--- app.py
+++ app.py
@@ -1,48 +1,87 @@
+import streamlit as st
+import  os
+from PIL import Image
+import pickle
 import numpy as np
 from numpy.linalg import norm
 import tensorflow
 from tensorflow.keras.preprocessing import image
-from tensorflow.keras.layers import GlobalMaxPool2D
-from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input
-import os
-from tqdm import tqdm
-# from PIL import Image
-import pickle
+from tensorflow.keras.layers import GlobalMaxPooling2D
+from keras.applications.vgg16 import VGG16,  preprocess_input
+from sklearn.neighbors import NearestNeighbors
+import cv2
 
 
-model = ResNet50(weights= 'imagenet', include_top= False, input_shape=(224,224,3))
-model.trainable= False
 
-model= tensorflow.keras.Sequential([model,GlobalMaxPool2D()])
 
-print(model.summary())
+feature_list = np.array(pickle.load(open('embedding.pkl', 'rb')))
+filenames = pickle.load(open('filenames.pkl', 'rb'))
 
-def extract_features(img_path,model):
-    img= image.load_img(img_path, target_size=(224,224))
-    img_array= image.img_to_array(img)
-    expanded_img_array= np.expand_dims(img_array, axis=0)
-    preprocess_img= preprocess_input(expanded_img_array)
-    result= model.predict(preprocess_img).flatten()
-    normalized_result= result/norm(result)
+model = VGG16(weights='imagenet',include_top=False,input_shape=(224,224,3))
+model.trainable = False
+
+model = tensorflow.keras.Sequential([
+    model,
+    GlobalMaxPooling2D()
+])
+
+
+
+st.title(' Ladies Fashion Recommender System')
+
+def save_uploaded_file(uploaded_file):
+    try:
+        with open(os.path.join('uploads', uploaded_file.name), 'wb') as f:
+            f.write(uploaded_file.getbuffer())
+        return 1
+    except:
+        return 0
+
+
+def feature_extraction(img_path, model):
+    img = image.load_img(img_path, target_size=(224, 224))
+    img_array = image.img_to_array(img)
+    expanded_img_array = np.expand_dims(img_array, axis=0)
+    preprocess_img = preprocess_input(expanded_img_array)
+    result = model.predict(preprocess_img).flatten()
+    normalized_result = result / norm(result)
 
     return normalized_result
 
-filenames= []
+def recommend( features, feature_list):
+    neighbors = NearestNeighbors(n_neighbors=6, algorithm='brute', metric='euclidean')
+    neighbors.fit(feature_list)
 
-for file in os.listdir('images'):
-    filenames.append(os.path.join('images',file))
+    distances, indices = neighbors.kneighbors([features])
 
-# print(len(filenames))
-# print(filenames[0:10])
-
-feature_list = []
-
-for file in tqdm(filenames):
-    feature_list.append(extract_features(file, model))
-
-# print(np.array(feature_list).shape)
-
-pickle.dump(feature_list,open('embeddings.pkl','wb'))
-pickle.dump(filenames,open('filenames.pkl','wb'))
+    return indices
 
 
+uploaded_file = st.file_uploader(" choose as image")
+if uploaded_file is not None:
+    if save_uploaded_file(uploaded_file):
+        display_img = Image.open(uploaded_file)
+        st.image(display_img)
+
+        features = feature_extraction(os.path.join("uploads", uploaded_file.name), model)
+        # st.text(features)
+
+
+        indices = recommend(features, feature_list)
+
+        col1, col2, col3, col4, col5 = st.columns(5)
+
+        with col1:
+            st.image(filenames[indices[0][0]])
+        with col2:
+            st.image(filenames[indices[0][1]])
+        with col3:
+            st.image(filenames[indices[0][2]])
+        with col4:
+            st.image(filenames[indices[0][3]])
+        with col5:
+            st.image(filenames[indices[0][4]])
+
+
+    else:
+        st.header("Som error occured in file upload")
